e2eaec0566380a8a7a90e71e1831996b4b44adbe
Chewbacca96 <lebedev582@gmail.com>
2016-09-06 13:35:08 +0300

    Changes
    Some changes.

---
+++ config.php
@@ -0,0 +1,3 @@
+<?php
+	$checkManuf = ['BMW', 'HONDA', 'KAWASAKI', 'SUZUKI', 'YAMAHA', 'APRILIA', 'ARCTIC CAT', 'BOMBARDIER', 'HARLEY DAVIDSON', 'BUELL', 'CAGIVA', 'DERBI', 'HUSABERG', 'GILERA', 'HUSQVARNA', 'HYOSUNG', 'KTM', 'MOTO GUZZI', 'MV Agusta', 'POLARIS', 'TRIUMPH', 'VICTORY'];
+	$checkBike = ['Motorrad', 'ATV/Quad', 'Roller'];
--- index.php
+++ index.php
@@ -1,70 +1,67 @@
 <?php
-	ini_set('max_execution_time', 900);
+	ini_set('max_execution_time', 0);
 	include 'PHP_Simple_HTML_DOM_Parser\simple_html_dom.php';
-
-	$checkManuf = array('BMW', 'HONDA', 'KAWASAKI', 'SUZUKI', 'YAMAHA', 'APRILIA', 'ARCTIC CAT', 'BOMBARDIER', 'HARLEY DAVIDSON', 'BUELL', 'CAGIVA', 'DERBI', 'HUSABERG', 'GILERA', 'HUSQVARNA', 'HYOSUNG', 'KTM', 'MOTO GUZZI', 'MV Agusta', 'POLARIS', 'TRIUMPH', 'VICTORY');
-	$checkBike = array('Motorrad', 'ATV/Quad', 'Roller');
+	include 'config.php';
 
 	$html = file_get_html('https://www.louis.de/en');
 	$manufValue = $html->find('select[id=bikedb-flyout-manufacturer]', 0)->find('option');
 
-	foreach($manufValue as $element) {
-		if (($element->value > 0) and in_array($element->innertext, $checkManuf)) {
-			//echo "$element->innertext - ";
+	foreach($manufValue as $manufElem) {
+		if (($manufElem->value > 0) && in_array($manufElem->innertext, $checkManuf)) {
+			//echo "$manufElem->innertext - ";
 
-			$bikeTypeList = file_get_html("https://www.louis.de/en/m/ajax/json/select-from-list?bike-selection-fieldset[manufacturer]=$element->value&bike-selection-fieldset[sortBySelect]=title&get=biketype");
+			$bikeTypeList = file_get_contents("https://www.louis.de/en/m/ajax/json/select-from-list?bike-selection-fieldset[manufacturer]=$manufElem->value&bike-selection-fieldset[sortBySelect]=title&get=biketype");
 			$bikeTypeList = json_decode($bikeTypeList, true);
 
-			$i = 1;
-			while ($i < count($bikeTypeList['options'])) {
-				if (in_array($bikeTypeList['options'][$i]['value'], $checkBike)) {
-					//echo $bikeTypeList['options'][$i]['value'] . ' - ';
+			array_shift($bikeTypeList['options']);
 
-					$bikeType = $bikeTypeList['options'][$i]['value'];
-					$engineSizeList = file_get_html("https://www.louis.de/en/m/ajax/json/select-from-list?bike-selection-fieldset[manufacturer]=$element->value&bike-selection-fieldset[biketype]=$bikeType&bike-selection-fieldset[sortBySelect]=title&get=capacity");
+			foreach ($bikeTypeList['options'] as $bikeElem) {
+				if (in_array($bikeElem['value'], $checkBike)) {
+					//echo $bikeElem['value'] . ' - ';
+
+					$bikeType = $bikeElem['value'];
+					$engineSizeList = file_get_contents("https://www.louis.de/en/m/ajax/json/select-from-list?bike-selection-fieldset[manufacturer]=$manufElem->value&bike-selection-fieldset[biketype]=$bikeType&bike-selection-fieldset[sortBySelect]=title&get=capacity");
 					$engineSizeList = json_decode($engineSizeList, true);
 
-					$j = 2;
-					while ($j < count($engineSizeList['options'])) {
-						//echo $engineSizeList['options'][$j]['value'] . ', ';
+					array_shift($engineSizeList['options']);
+					array_shift($engineSizeList['options']);
 
-						$engineSize = $engineSizeList['options'][$j]['value'];
-						$data = file_get_html("https://www.louis.de/en/m/ajax/json/select-from-list?bike-selection-fieldset[manufacturer]=$element->value&bike-selection-fieldset[biketype]=$bikeType&bike-selection-fieldset[capacity]=$engineSize&bike-selection-fieldset[sortBySelect]=title&sortby=title&get=bikes");
+					foreach ($engineSizeList['options'] as $engineElem) {
+						//echo $engineElem['value'] . ', ';
+
+						$engineSize = $engineElem['value'];
+						$data = file_get_contents("https://www.louis.de/en/m/ajax/json/select-from-list?bike-selection-fieldset[manufacturer]=$manufElem->value&bike-selection-fieldset[biketype]=$bikeType&bike-selection-fieldset[capacity]=$engineSize&bike-selection-fieldset[sortBySelect]=title&sortby=title&get=bikes");
 						$data = json_decode($data, true);
 
-						$k = 1;
-						while ($k < count($data['options'])) {
-							//echo $data['options'][$k]['title'] . '<br>';
+						array_shift($data['options']);
 
-							$manufStr = substr($data['options'][$k]['title'], 0, strripos($data['options'][$k]['title'], ', year'));
+						foreach ($data['options'] as $dataElem) {
+							//echo $data['options'][$k]['title'] . '<br>';
+							$title = $dataElem['title'];
+
+							$manufStr = substr($title, 0, strripos($title, ', year'));
 							//echo $manufStr . '<br>';
 
-							$yearStart = substr($data['options'][$k]['title'], strripos($data['options'][$k]['title'], ', year') + 17, 4);
+							$yearStart = substr($title, strripos($title, ', year') + 17, 4);
 							//echo $yearStart . '<br>';
 
-							if (substr($data['options'][$k]['title'], strripos($data['options'][$k]['title'], ' - ') + 3, 1) == '1') {
-								$yearEnd = substr($data['options'][$k]['title'], strripos($data['options'][$k]['title'], ' - ') + 3, 4);
+							if (substr($title, strripos($title, ' - ') + 3, 1) == '1') {
+								$yearEnd = substr($title, strripos($title, ' - ') + 3, 4);
 							} else {
-								$yearEnd = '-';
+								$yearEnd = null;
 							}
 							//echo $yearEnd . '<br>';
 
-							$ramaStr = substr($data['options'][$k]['title'], strripos($data['options'][$k]['title'], '(') + 1, strripos($data['options'][$k]['title'], ')') - strripos($data['options'][$k]['title'], '(') - 1);
+							$ramaStr = substr($title, strripos($title, '(') + 1, strripos($title, ')') - strripos($title, '(') - 1);
 							//echo $ramaStr . '<br>';
 
-							//echo "$manufStr, $yearStart, $yearEnd, $ramaStr <br>";
-							$k++;
+							echo "$manufStr, $yearStart, $yearEnd, $ramaStr <br>";
 						}
-
-						//echo '<br>';
-						$j++;
+						echo '<br>';
 					}
-
 					//echo '<br>';
 				}
-				$i++;
 			}
 			//echo "<br>";
 		}
 	}
-?>

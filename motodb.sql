1073fa3989c566549717071e00c522405f1ea658
Chewbacca96 <lebedev582@gmail.com>
2016-09-15 17:40:28 +0300

    Change
    Change

--- motodb.sql
+++ motodb.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Хост: 127.0.0.1
--- Время создания: Сен 14 2016 г., 15:29
+-- Время создания: Сен 15 2016 г., 18:38
 -- Версия сервера: 5.5.25
 -- Версия PHP: 5.3.3
 
@@ -31,7 +31,7 @@ CREATE TABLE IF NOT EXISTS `t_mark` (
   `value` text NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `mark_value` (`value`(255))
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -46,14 +46,14 @@ CREATE TABLE IF NOT EXISTS `t_model` (
   `code` int(11) NOT NULL,
   `model` text NOT NULL,
   `capacity` int(11) NOT NULL,
-  `year_start` int(11) NOT NULL,
+  `year_start` int(11) DEFAULT NULL,
   `year_end` int(11) DEFAULT NULL,
   `frame` text NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `idmodel` (`code`),
   KEY `fk_mark_id` (`mark_id`),
   KEY `fk_type_id` (`type_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2165 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -66,7 +66,18 @@ CREATE TABLE IF NOT EXISTS `t_type` (
   `value` text NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `type_value` (`value`(255))
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+--
+-- Ограничения внешнего ключа сохраненных таблиц
+--
+
+--
+-- Ограничения внешнего ключа таблицы `t_model`
+--
+ALTER TABLE `t_model`
+  ADD CONSTRAINT `t_model_ibfk_1` FOREIGN KEY (`mark_id`) REFERENCES `t_mark` (`id`),
+  ADD CONSTRAINT `t_model_ibfk_2` FOREIGN KEY (`type_id`) REFERENCES `t_type` (`id`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
